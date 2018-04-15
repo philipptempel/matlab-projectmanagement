e4df2737ecd4620461efa5d8d43e312626ad233f
Philipp Tempel <mail@philipptempel.me>
2018-04-15 15:41:27 +0200

    Add test for dependencies

--- +projman/+tests/project.m
+++ +projman/+tests/project.m
@@ -22,6 +22,8 @@ classdef project < matlab.unittest.TestCase
             % Make an empty project
             p = projman.project(pwd);
             
+            
+            %%% Assertion
             % Check correct object type is returned
             tc.assertThat(p, IsInstanceOf('projman.project'));
             
@@ -39,9 +41,42 @@ classdef project < matlab.unittest.TestCase
             % Make an empty project
             p = projman.project(pwd);
             
+            
+            %%% Assertion
             % Check correct object type is returned
             tc.assertThat(p.Exists, IsTrue);
+            % Check the project exists (since the path exists, it must exist,
+            % too)
             tc.assertThat(exist(p), IsTrue);
+            
+        end
+        
+        
+        function testResolveDependencies(tc)
+            %% TESTRESOLVEDEPENDENCIES
+            
+            
+            % Import constraints
+            import matlab.unittest.constraints.IsTrue;
+            import matlab.unittest.constraints.IsEqualTo;
+            import matlab.unittest.constraints.IsSameHandleAs;
+            
+            % Make some projects
+            p(1) = projman.project(fullfile(pwd, 'independent'), 'Project 1');
+            p(2) = projman.project(fullfile(pwd, 'base'), 'Project 2');
+            p(3) = projman.project(fullfile(pwd, 'base', 'dependent_on_2'), 'Project 3', p(2));
+            p(4) = projman.project(fullfile(pwd, 'base', 'dependent_on_2'), 'Project 4', p(2));
+            p(5) = projman.project(fullfile(pwd, 'base', 'dependent_on_4'), 'Project 5', p(4));
+            
+            % Resolve the dependencies
+            deps = p(5).resolve_dependencies();
+            
+            
+            %%% Assertion
+            % List of dependencies must be equal to this
+            tc.assertThat(deps, IsEqualTo([p(2), p(3)]));
+            % List of dependencies must be the same handles as
+            tc.assertThat(deps, IsSameHandleAs([p(2), p(3)]));
             
         end
         
