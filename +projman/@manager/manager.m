4720f324cf095802903faa4954cdb8c1de08c4b4
Philipp Tempel <mail@philipptempel.me>
2018-04-29 18:46:18 +0200

    Add support to find similar projects if the requested one cannot be found

--- +projman/@manager/manager.m
+++ +projman/@manager/manager.m
@@ -55,6 +55,86 @@ classdef manager < handle
     end
     
     
+    %% STATIC PROTECTED METHODS
+    methods ( Static, Access = protected )
+        
+        function d = strdist(r, b, krk, cas)
+            %% STRDIST computes distances between strings
+            %
+            % d=strdist(r,b,krk,cas) computes Levenshtein and editor distance 
+            % between strings r and b with use of Vagner-Fisher algorithm.
+            %    Levenshtein distance is the minimal quantity of character
+            % substitutions, deletions and insertions for transformation
+            % of string r into string b. An editor distance is computed as 
+            % Levenshtein distance with substitutions weight of 2.
+            % d=strdist(r) computes numel(r);
+            % d=strdist(r,b) computes Levenshtein distance between r and b.
+            % If b is empty string then d=numel(r);
+            % d=strdist(r,b,krk)computes both Levenshtein and an editor distance
+            % when krk=2. d=strdist(r,b,krk,cas) computes a distance accordingly 
+            % with krk and cas. If cas>0 then case is ignored.
+            % 
+            % Example.
+            %  disp(strdist('matlab'))
+            %     6
+            %  disp(strdist('matlab','Mathworks'))
+            %     7
+            %  disp(strdist('matlab','Mathworks',2))
+            %     7    11
+            %  disp(strdist('matlab','Mathworks',2,1))
+            %     6     9
+
+            switch nargin
+               case 1
+                  d = numel(r);
+                  return
+               case 2
+                  krk = 1;
+                  bb = b;
+                  rr = r;
+               case 3
+                   bb = b;
+                   rr = r;
+               case 4
+                  bb = b;
+                  rr = r;
+                  if cas > 0
+                     bb = upper(b);
+                     rr = upper(r);
+                  end
+            end
+
+            if krk ~= 2
+               krk = 1;
+            end
+
+            d = zeros(1, 0);
+            luma = numel(bb);
+            lima = numel(rr);
+            lu1 = luma + 1;
+            li1 = lima + 1;
+            dl = zeros([lu1, li1]);
+            dl(1,:) = 0:lima;
+            dl(:,1) = 0:luma;
+            % Distance
+            for krk1 = 1:krk
+                for ii = 2:lu1
+                    bbi = bb(ii-1);
+                    for ij = 2:li1
+                        kr = krk1;
+                        if strcmp(rr(ij-1),bbi)
+                            kr = 0;
+                        end
+                        dl(ii,ij) = min([dl(ii-1,ij-1) + kr, dl(ii-1,ij) + 1, dl(ii,ij-1) + 1]);
+                    end
+                end
+                d = horzcat(d, dl(end,end));
+            end
+        end
+        
+    end
+    
+    
     %% GENERAL METHODS
     methods
         
@@ -77,13 +157,22 @@ classdef manager < handle
                 % Find matching names
                 idxMatches = ismember({this.Projects.Name}, name);
                 
-                % Make sure we found a project
+                % Make sure we found any project
                 assert(any(idxMatches), 'PHILIPPTEMPEL:PROJMAN:PROJMAN:MANAGER:FIND:ProjectNotFound', 'Project could not be found because it does not exist or names are too ambigious');
                 
                 % And return the data
                 p = this.Projects(idxMatches);
             catch me
-                throwAsCaller(me);
+                % No match, so let's suggest projects based on their string
+                % distance
+                pclose = this.closest_projects(name);
+                
+                % Found similarly sounding projects?
+                if ~isempty(pclose)
+                    throwAsCaller(addCause(MException('PHILIPPTEMPEL:PROJMAN:PROJMAN:MANAGER:FIND:ProjectNotFound', 'Project could not be found. Did you maybe mean one of the following projects?\n%s', strjoin(arrayfun(@(pp) pp.Name, pclose, 'UniformOutput', false), '\n')), me));
+                else
+                    throwAsCaller(addCause(MException('PHILIPPTEMPEL:PROJMAN:PROJMAN:MANAGER:FIND:ProjectNotFound', 'Project could not be found. Make sure there is no typo in the name and that the project exists.'), me));
+                end
             end
             
         end
@@ -226,6 +315,25 @@ classdef manager < handle
                 % Init empty projects array
                 this.Projects = projman.project.empty(1, 0);
             end
+            
+        end
+        
+        
+        function ps = closest_projects(this, name)
+            %% CLOSEST_PROJECTS finds the projects with a name closest to the needle
+            
+            
+            % Get the distance between the needle and all other projects' names
+            dists = cellfun(@(n) projman.manager.strdist(name, n), {this.Projects.Name});
+            % Sort the distances from shortest to longest
+            [dists, sortidx] = sort(dists);
+            
+            % Now get all projects whose name distance is smaller than 10 (some
+            % random/arbitrary value)
+            sortidx = sortidx(dists < 10);
+            
+            % And return these projects
+            ps = this.Projects(sortidx);
             
         end
         
